36600406586a6be6e839c0e46152d660e67db2be
Ian <152772a@mymail.nyp.edu.sg>
2016-11-22 00:26:53 +0800

    i

--- app/src/main/java/com/example/ian/practtestq2/MainActivity.java
+++ app/src/main/java/com/example/ian/practtestq2/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.ian.practtestq2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<CharSequence> ad;
+    Spinner sp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp = (Spinner) findViewById(R.id.ct);
+
+        ad = ArrayAdapter.createFromResource(this, R.array.contact, android.R.layout.simple_spinner_dropdown_item);
+        ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(ad);
+
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/ian/practtestq2/Toast.java
@@ -0,0 +1,10 @@
+package com.example.ian.practtestq2;
+
+/**
+ * Created by Ian on 22/11/2016.
+ */
+
+public class Toast {
+
+
+}
