a1597f5257851ee71d30f9c033a9e9adaa6d9609
Ian <152772a@mymail.nyp.edu.sg>
2016-11-22 11:52:19 +0800

    i

--- app/src/main/java/com/example/ian/practtestq2/MainActivity.java
+++ app/src/main/java/com/example/ian/practtestq2/MainActivity.java
@@ -8,10 +8,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import static android.app.ProgressDialog.show;
+
 public class MainActivity extends AppCompatActivity {
 
     ArrayAdapter<CharSequence> ad;
     Spinner sp;
+    boolean test = false;
 
 
     @Override
@@ -25,17 +28,22 @@ public class MainActivity extends AppCompatActivity {
         ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp.setAdapter(ad);
 
-        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+    sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+if(test==true){
+            Toast.makeText(getApplicationContext(), sp.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();}
+            test=true;
 
-            }
+        }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
 
-            }
-        });
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    });
+
 
 
     }
