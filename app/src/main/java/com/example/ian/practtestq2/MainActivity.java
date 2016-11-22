4778f3d1f06de9832d1d37a3be6feafbf70fa0dd
Ian <152772a@mymail.nyp.edu.sg>
2016-11-22 20:53:54 +0800

    i

--- app/src/main/java/com/example/ian/practtestq2/MainActivity.java
+++ app/src/main/java/com/example/ian/practtestq2/MainActivity.java
@@ -28,22 +28,25 @@ public class MainActivity extends AppCompatActivity {
         ad.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp.setAdapter(ad);
 
-    sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-        @Override
-        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-if(test==true){
-            Toast.makeText(getApplicationContext(), sp.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();}
-            test=true;
-
-        }
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (test == true) {
 
 
-        @Override
-        public void onNothingSelected(AdapterView<?> adapterView) {
+                    Toast.makeText(getApplicationContext(), sp.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+                }
+                test = true;
 
-        }
-    });
+            }
 
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+
+            }
+        });
 
 
     }
